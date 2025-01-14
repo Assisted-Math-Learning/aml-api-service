5ab56f924d0560ab856e9099da572ca9d0cd5192
vipin_chaudhary-dl <vipin.chaudhary@devslane.com>
2025-01-14 11:59:58 +0530

    removed leading zeroes from FIB answers

--- src/utils/string.util.ts
+++ src/utils/string.util.ts
@@ -1,3 +1,7 @@
 export const replaceAt = (str: string, index: number, replacement: string) => {
   return str.substring(0, index) + replacement + str.substring(index + replacement.length);
 };
+
+export const replaceLeadingZeroes = (str?: string) => {
+  return str?.replace(/^0+(?=[1-9]|0$)/g, '');
+};
