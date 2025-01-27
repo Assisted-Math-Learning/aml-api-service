97cd40b170f6495f325f0fe7a445c1b6fe328734
karan-devslane <karan.budhwani@devslane.com>
2025-01-27 16:01:54 +0530

    Added prod hotfix migrations

--- src/routes/dataMigration.route.ts
+++ src/routes/dataMigration.route.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import { setDataToRequestObject } from '../middlewares/setDataToReqObj';
 import { learnerTaxonomyToColumns } from '../controllers/dataMigrations/learnerTaxonomyToColumns';
 import createQuestionSetQuestionMapping from '../controllers/dataMigrations/createQuestionSetQuestionMapping';
+import fibTypeUpdate from '../controllers/dataMigrations/fibTypeUpdate';
+import updateFibScores from '../controllers/dataMigrations/updateFibScores';
 
 export const dataMigrations = express.Router();
 
 dataMigrations.post('/learner-taxonomy-to-columns', setDataToRequestObject('api.migration.learnerTaxonomyToColumns'), learnerTaxonomyToColumns);
 
 dataMigrations.post('/question-set-question-mapping', setDataToRequestObject('api.migration.learnerTaxonomyToColumns'), createQuestionSetQuestionMapping);
+
+dataMigrations.post('/fib-type-update', fibTypeUpdate);
+
+dataMigrations.post('/update-fib-scores', updateFibScores);
